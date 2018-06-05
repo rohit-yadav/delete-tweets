513d919c626344f0902490e66acd20ed977da52a
Rohit Yadav <rohityadav5479@gmail.com>
2018-06-05 18:29:48 +0530

    add html and py file to track

---
+++ delete_tweets.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# # Analysing and Deleting Tweets
+
+# ### Asking the right Quesitons
+# 
+#    * Wordcloud
+#    * Number of tweets each month
+#    * No of Tweets during the day of the week
+#    * No of Tweets during the day - Time during I tweet most
+#    * Impressions and profile visit visualization.
+#    * All the hash tags I have used so far
+#    * Delete all tweets - get user authentication to delete
+
+# In[1]:
+
+
+# Importing the modules
+import time
+import urllib
+from bs4 import BeautifulSoup
+import requests
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
